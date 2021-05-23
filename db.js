3efa6bdbb645cfe9dc979a6c8150d55bfe152e2f
Oleg <drobotoo@gmail.com>
2021-05-23 19:30:51 +0300

    fix: cleared code

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'kbpf18072011', {
+const sequelize = new Sequelize('gamedb', 'postgres', 'password', {
     host: 'localhost',
     dialect: 'postgres',
     port: "5433"
 })
 const User = require('./models/user')(sequelize,Sequelize);
 const Game = require('./models/game')(sequelize,Sequelize);
-//console.log('sequelize ', sequelize);
+
 
 sequelize.sync().then(
-	//console.log('sequelize'),
+
     function success() {
         console.log("Connected to DB");
     },
